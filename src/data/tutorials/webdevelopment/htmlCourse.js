80100ef90ae5c4fa16083941c2d0a84f5093c035
techysiddhant <sidjain8000@gmail.com>
2023-05-09 12:49:30 +0530

    add the lecture link into the html tutorial file

--- src/data/tutorials/webdevelopment/htmlCourse.js
+++ src/data/tutorials/webdevelopment/htmlCourse.js
@@ -8,6 +8,8 @@ export const htmlCourse = [{
                 lectureName: "Introduction to HTML, CSS, JavaScript &amp; How websites work?",
                 lectureUrl: "https://www.youtube.com/embed/6mbwJ2xhgzM?list=PLu0W_9lII9agiCUZYRsvtGTXdxkzPyItg",
                 lectureDescription: "",
+                lectureLink:"https://www.codewithharry.com/videos/web-development-in-hindi-1/"
+                
 
             },
             {
@@ -15,42 +17,49 @@ export const htmlCourse = [{
                 lectureName: "HTML Tutorial: Installing VS Code &amp; Live Server",
                 lectureUrl: "https://www.youtube.com/embed/TeZdo8mx0gc?list=PLu0W_9lII9agiCUZYRsvtGTXdxkzPyItg",
                 lectureDescription: "",
+                lectureLink:"https://www.codewithharry.com/videos/web-development-in-hindi-2/"
             },
             {
                 lectureId: 2,
                 lectureName: "HTML Tutorial: Basic Structure of a Website",
                 lectureUrl: "https://www.youtube.com/embed/IA8JWGP13dI?list=PLu0W_9lII9agiCUZYRsvtGTXdxkzPyItg",
                 lectureDescription: "",
+                lectureLink:"https://www.codewithharry.com/videos/web-development-in-hindi-3/"
             },
             {
                 lectureId: 3,
                 lectureName: "HTML Tutorial: Title, Script, Link &amp; Meta Tags",
                 lectureUrl: "https://www.youtube.com/embed/EZCc_4abdcE?list=PLu0W_9lII9agiCUZYRsvtGTXdxkzPyItg",
                 lectureDescription: "",
+                lectureLink:"https://www.codewithharry.com/videos/web-development-in-hindi-4/"
             },
             {
                 lectureId: 4,
                 lectureName: "HTML Tutorial: Headings &amp; Paragraphs",
                 lectureUrl: "https://www.youtube.com/embed/ulv_q6-b7uI?list=PLu0W_9lII9agiCUZYRsvtGTXdxkzPyItg",
                 lectureDescription: "",
+                lectureLink:"https://www.codewithharry.com/videos/web-development-in-hindi-5/"
             },
             {
                 lectureId: 5,
                 lectureName: "HTML Tutorial: Img and Anchor tags",
                 lectureUrl: "https://www.youtube.com/embed/z6H22xGAZEA?list=PLu0W_9lII9agiCUZYRsvtGTXdxkzPyItg",
                 lectureDescription: "",
+                lectureLink:"https://www.codewithharry.com/videos/web-development-in-hindi-6/"
             },
             {
                 lectureId: 6,
                 lectureName: "HTML Tutorial: Lists and Tables",
                 lectureUrl: "https://www.youtube.com/embed/N69xumSjg5Q?list=PLu0W_9lII9agiCUZYRsvtGTXdxkzPyItg",
                 lectureDescription: "",
+                lectureLink:"https://www.codewithharry.com/videos/web-development-in-hindi-7/"
             },
             {
                 lectureId: 7,
                 lectureName: "HTML Tutorial: Forms &amp; Input Tags",
                 lectureUrl: "https://www.youtube.com/embed/KqJikDzb3l4?list=PLu0W_9lII9agiCUZYRsvtGTXdxkzPyItg",
                 lectureDescription: "",
+                lectureLink:"https://www.codewithharry.com/videos/web-development-in-hindi-8/"
 
             },
             {
@@ -58,23 +67,28 @@ export const htmlCourse = [{
                 lectureName: "HTML Tutorial: Inline &amp; Block Elements",
                 lectureUrl: "https://www.youtube.com/embed/DFT9qxVCF6k?list=PLu0W_9lII9agiCUZYRsvtGTXdxkzPyItg",
                 lectureDescription: "",
+                lectureLink:"https://www.codewithharry.com/videos/web-development-in-hindi-9/"
             },
             {
                 lectureId: 9,
                 lectureName: "HTML Tutorial: Ids &amp; Classes in HTML",
                 lectureUrl: "https://www.youtube.com/embed/BucLTOfLQsk?list=PLu0W_9lII9agiCUZYRsvtGTXdxkzPyItg",
                 lectureDescription: "",
+                lectureLink:"https://www.codewithharry.com/videos/web-development-in-hindi-10/"
             },
             {
                 lectureId: 10,
                 lectureName: "HTML Tutorial: HTML Entities ",
                 lectureUrl: "https://www.youtube.com/embed/gw1efv5WF_Q?list=PLu0W_9lII9agiCUZYRsvtGTXdxkzPyItg",
                 lectureDescription: "",
+                lectureLink:"https://www.codewithharry.com/videos/web-development-in-hindi-11/"
             },
             {
                 lectureId: 11,
                 lectureName: "HTML Tutorial: Semantic Tags in HTML",
                 lectureUrl: "https://www.youtube.com/embed/FKfsmV6otEM?list=PLu0W_9lII9agiCUZYRsvtGTXdxkzPyItg",
+                lectureDescription: "",
+                lectureLink:"https://www.codewithharry.com/videos/web-development-in-hindi-12/"
             },
         ]
     },
@@ -88,12 +102,15 @@ export const htmlCourse = [{
                 lectureName: "Discover the Magic Behind Websites: How They Work",
                 lectureUrl: "https://www.youtube.com/embed/wr9-UL9cWUk?list=PLt5mNkGuWcuVa4GNEsDbH5L6ONaIMsbMa",
                 lectureDescription: "In this video, We are launching a full-stack web development course in Hindi. This is the introduction video of full stack web development course. Discover the Magic Behind Websites: How They Work ",
+                lectureLink:""
+
             },
             {
                 lectureId: 1,
                 lectureName: "HTML Elements: Anchor, IMG, VIDEO, AUDIO, LIST, TABLE",
                 lectureUrl: "https://www.youtube.com/embed/L0l8acdWh2A?list=PLt5mNkGuWcuVa4GNEsDbH5L6ONaIMsbMa",
                 lectureDescription: "In this video, We will learn about HTML Elements: Anchor, IMG, VIDEO, AUDIO, LIST, TABLE. ",
+                lectureLink:""
 
             },
             {
@@ -101,6 +118,7 @@ export const htmlCourse = [{
                 lectureName: "Mastering HTML Forms, Class &amp; ID, Semantic Tag",
                 lectureUrl: "https://www.youtube.com/embed/Hg8N6LE0MaQ?list=PLt5mNkGuWcuVa4GNEsDbH5L6ONaIMsbMa",
                 lectureDescription: "In this video, We will learn about Mastering HTML Forms, Class &amp; ID, Semantic Tag. ",
+                lectureLink:""
             },
 
         ]
